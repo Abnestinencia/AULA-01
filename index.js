668e9efbfe491d6c2c54af5135545f04728495e7
Abnestinencia <abnermoraesdp@gmail.com>
2024-12-08 20:45:10 -0300

    a

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const app = express();
 
 function paginaInicial(req, res){ //t
-    resposta.send(`<h1>Seja bem vindo!</h1>
+    res.send(`<h1>Seja bem vindo!</h1>
                    <br/>
                    <h2>Primeiros passos para desenvolvimento de aplicação web com NodeJs</h2>
                    <h3>Pagina Inicial</h3>
