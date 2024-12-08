bd1b95160626277040447dadf90c777309ebe0e8
Abnestinencia <abnermoraesdp@gmail.com>
2024-12-08 17:07:15 -0300

    Definindo a pagina inicial da aplicação web

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const host = "0.0.0.0";
+const port = 3000;
+
+const app = express();
+
+function paginaInicial(requisição, resposta){
+    resposta.send(`<h1>Seja bem vindo!</h1>
+                   <br/>
+                   <h2>Primeiros passos para desenvolvimento de aplicação web com NodeJs</h2>
+                   <h3>Pagina Inicial</h3>
+        `);
+}
+
+app.get("/" paginaInicial);
+
+app.listen(port, host, () => {
+    console.log("Servidor em execução http://" + host + ":" + port);
+});
